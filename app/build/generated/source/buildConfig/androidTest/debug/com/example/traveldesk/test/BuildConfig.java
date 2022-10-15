d8f09a65f966a0b02e9c990e4214fee1206261a9
indira113 <indiravik113@gmail.com>
2022-10-15 17:52:03 +0530

    added

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/traveldesk/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.traveldesk.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.traveldesk.test";
+  public static final String BUILD_TYPE = "debug";
+}
